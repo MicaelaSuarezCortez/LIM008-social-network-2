5447c77aee232510b45513725f90d9c4350fb389
MicaelaSuarezCortez <micaelasuarezco@gmail.com>
2019-02-19 19:36:32 -0500

    implementing register function

--- src/lib/controllers/controller-firebase.js
+++
@@ -1,7 +0,0 @@
-export const signUp = (email, password) => {
-  firebase.auth().createUserWithEmailAndPassword(email, password);
-};
-
-export const signIn = (email, password) =>
-  firebase.auth().signInWithEmailAndPassword(email, password);
-
